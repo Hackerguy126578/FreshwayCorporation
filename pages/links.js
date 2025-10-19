d8246f74052af1a82deef6a011bf9bcd761a0d03
Hackerguy126578 <135268993+Hackerguy126578@users.noreply.github.com>
2025-10-19 20:42:27 +0200

    Update links.js

--- pages/links.js
+++ pages/links.js
@@ -1,6 +1,14 @@
 import Link from 'next/link'
 
 export default function Links() {
+  const links = [
+    { name: "Site", url: "https://sites.google.com/view/freshwayroblox-corporation/homepage" },
+    { name: "Status Page", url: "https://freshway-corporation.betteruptime.com/" },
+    { name: "Main Game", url: "https://www.roblox.com/games/105961436004944/OPENING-CEREMONY-Freshway-Corporation" },
+    { name: "Training Center", url: "https://www.roblox.com/games/122026789399591/Training-Center" },
+    { name: "Application Center", url: "https://www.roblox.com/games/111969553355039/Freshway-PLC-Application-Center" },
+  ];
+
   return (
     <>
       <header>
@@ -16,11 +24,14 @@ export default function Links() {
       </header>
       <main>
         <h2>Useful Links</h2>
-        <ul>
-          <li><a href="https://www.freshway.com">Company Website</a></li>
-          <li><a href="https://www.freshway.com/blog">Blog</a></li>
-          <li><a href="https://www.freshway.com/contact">Contact Us</a></li>
-        </ul>
+        <div>
+          {links.map((link, index) => (
+            <div key={index} className="card">
+              <h3>{link.name}</h3>
+              <a href={link.url} className="link-button" target="_blank" rel="noopener noreferrer">Go</a>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   )
